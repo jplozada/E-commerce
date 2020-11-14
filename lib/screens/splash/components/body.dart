a2a12947fdd7c4b725236a8318988b84dde2bfb8
jplozada <juxnpx@gmail.com>
2020-11-13 22:03:39 -0400

    Splash screen: subtitle and image carousel

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_app/constants.dart';
 import 'package:e_commerce_app/size_config.dart';
 import 'package:flutter/material.dart';
+import '../components/splash_content.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,33 +9,37 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to Tokoto, Let’s shop!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text":
+          "We help people conect with store \naround United State of America",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with us",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-          child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Text("TOKOTO", style: TextStyle(
-                    fontSize: getProportionateScreenWidth(36),
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.bold
-                  )),
-                  Text("Welcome to Tokoto, Let's shop")
-                ],
-              )
-            ),
-            Expanded(
-              flex: 2,
-              child: SizedBox()
-            ),
-          ],
-        )
-      )
-    );
+        child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Expanded(
+                    flex: 3,
+                    child: PageView.builder(
+                        itemCount: splashData.length,
+                        itemBuilder: (context, index) => SplashContent(
+                            image: splashData[index]["image"],
+                            text: splashData[index]["text"]))),
+                Expanded(flex: 2, child: SizedBox()),
+              ],
+            )));
   }
 }
